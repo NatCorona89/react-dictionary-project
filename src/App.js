145eeae93a931ea8b88bd76682e8fe7668244e99
NatCorona89 <nataliegilliam@outlook.com>
2021-12-21 17:34:06 -0800

    Added SheCodes logo to header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import logo from "./logo.png";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello!!</header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
     </div>
   );
 }
