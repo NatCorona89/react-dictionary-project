0cc3e52c17dcb0892aee64c7d7faf6cbc519225c
NatCorona89 <nataliegilliam@outlook.com>
2021-12-23 14:52:38 -0800

    Created synonyms component, added strings to select through them

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  console.log(props.synonyms);
   if (props.synonyms) {
-    return "Hello from synonyms";
+    return (
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
+    );
   } else {
     return null;
   }
